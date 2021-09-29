4bef1d603f9bbf8525ce1024b6906536781b0f4c
Mir ayon Elahi <mirayonelahi@Mirs-MBP.hitronhub.home>
2021-09-29 02:30:55 -0700

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,114 @@
+
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// Message from server
+socket.on('message', (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+socket.on('createPoll',(message)=>{
+  console.log("from"+message)
+  createPoll(message)
+}
+);
+
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get message text
+  let msg = e.target.elements.msg.value;
+
+  msg = msg.trim();
+
+  if (!msg) {
+    return false;
+  }
+
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+  socket.emit('createPoll', msg);
+
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  const p = document.createElement('p');
+  p.classList.add('meta');
+  p.innerText = message.username;
+  p.innerHTML += `<span>${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement('p');
+  para.classList.add('text');
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+}
+
+//Prompt the user before leave chat room
+document.getElementById('leave-btn').addEventListener('click', () => {
+  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+  if (leaveRoom) {
+    window.location = '../index.html';
+  } else {
+  }
+});
+
+function createPoll(message){
+  const div = document.createElement('div')
+  div.classList.add('createPoll')
+  div.innerHTML=`<form action="/">
+  <p>Poll Created By:  ${message.username}?</p>
+  <p>${message.text}?</p>
+  <input type="radio" id="yes" name="fav_language" value="Agreed">
+  <label for="yes">Agreed</label><br>
+  <input type="radio" id="no" name="fav_language" value="Disagreed">
+  <label for="no">Disagreed</label><br>
+
+  <input type="submit" value="Submit">
+</form>`;
+  document.querySelector('.chat-messages').appendChild(div)
+}
